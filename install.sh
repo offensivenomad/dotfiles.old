9522035a1d389031bae460363ce150ee47deed2d
offensivenomad <58550032+offensivenomad@users.noreply.github.com>
2020-03-06 17:10:12 +1100

    Error debugged \& fixed

--- install.sh
+++ install.sh
@@ -7,12 +7,17 @@
 H="$HOME"
 D="$HOME"/.dotfiles
 
+PYTHON_VERSION=$(python --version)
+
+POWERLINE_COLORSCHEME="$HOME/.local/lib/python3.8/site-packages/powerline/config_files/colorschemes/default.json"
+
 cp "$H"/.bashrc "$H"/.bashrc.bak
 ln -snf "$D"/bashrc "$H"/.bashrc
 
 cp -av "$H"/.config/* "$D"/config/
 rm -rf "$H"/.config
 ln -snf "$D"/config "$H"/.config
+ln -snf "$D"/profile "$H"/.profile
 
 ln -snf "$D"/bash.d "$H"/.bash.d
 ln -snf "$D"/scripts "$H"/.scripts
