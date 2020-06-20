2d4ee5499aa44e53b2607245386b0978910cb992
offensivenaomd <58550032+offensivenomad@users.noreply.github.com>
2020-06-21 03:50:04 +1000

    Error debugged \& fixed

--- bash.d/backup.sh
+++ bash.d/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 backup() {
-	rsync -avz --progress --exclude-from='/root/.backup-exclusions' /root /mnt/backup/
-	rsync -avz --progress --exclude-from='/home/loki/.backup-exclusions' /home/loki /mnt/backup/
+#	sudo rsync -avz --progress --include-from='/root/.backup-exclusions' /root /mnt/backup/
+	sudo rsync -avrlhz --progress --exclude-from='/home/loki/.dotfiles/backup-exclusions' /home/loki /mnt/backup/
 }
